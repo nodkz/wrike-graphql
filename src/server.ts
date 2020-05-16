16601ddd75a843679eef758223d607ce228343e5
nodkz <pavel.chertorogov@gmail.com>
2020-05-16 23:58:42 +0600

    add graphql-voyager

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,32 @@
 require('module-alias').addAlias('app', __dirname);
 
-import { ApolloServer } from 'apollo-server';
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { express as voyagerMiddleware } from 'graphql-voyager/middleware';
 import schema from 'app/schema';
 
-const server = new ApolloServer({
+const PORT = parseInt(process.env.PORT || '3000');
+const app = express();
+
+const apolloServer = new ApolloServer({
   schema,
 });
 
-server.listen(parseInt(process.env.PORT || '3000')).then(({ url }) => {
-  console.log(`🚀 Server ready at ${url}`);
+app.use('/voyager', voyagerMiddleware({ endpointUrl: apolloServer.graphqlPath }));
+
+app.use(
+  apolloServer.getMiddleware({
+    path: '/',
+    disableHealthCheck: true,
+    cors: {
+      credentials: true,
+      origin: (origin: string | undefined, callback: any) => {
+        callback(null, true);
+      },
+    },
+  })
+);
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server ready at http://localhost:${PORT}`);
 });
